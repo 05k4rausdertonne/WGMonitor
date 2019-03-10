ee45e11f143f4b9bf1fda24179af04d72053be6b
05k4rausdertonne <oskarflath@gmail.com>
2019-03-10 17:22:33 +0100

    Set up node.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var sys = require('util');
+var path = require('path');
+
+app.use( express.static( path.join(__dirname, "public") ) );
+
+/*http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);*/
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
